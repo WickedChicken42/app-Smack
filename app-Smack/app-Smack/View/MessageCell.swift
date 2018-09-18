6e214d48938cadae6c838469810df0e972b0edd1
James Ullom <jamesullom@Jamess-MacBook-Pro-2.local>
2018-09-18 13:07:08 -0600

    Added support for formatting the Timestamp in the messages and displaying it.

--- app-Smack/app-Smack/View/MessageCell.swift
+++ app-Smack/app-Smack/View/MessageCell.swift
@@ -29,5 +29,25 @@ class MessageCell: UITableViewCell {
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: messageItem.avatarColor)
         // Need to find out why this didn't work
         //userImage.backgroundColor = messageItem.avatarUIColor
+        
+        // ISO 86-01
+        // 2017-07-13T21:49:25.590Z
+        // YYYY-MM-DDThh:mm:ss.mmm
+        guard var isoDate = messageItem.messageTimeStamp else { return }
+        // Getting the index starting from the end and backign up 5 spots (2017-07-13T21:49:25)
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        // Formastting the date while appending the ending Z which is needed but was not included in our substring (2017-07-13T21:49:25Z)
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
     }
 }
